d62b6f97e5aaa9003372d2f62098c79d9810c20d
Le Ngoai Ngu <ngunl@capichiapp.com>
2022-04-07 12:16:14 +0700

    keep in mind when compare

---
+++ src/lectures/02-04-compare.ts
@@ -0,0 +1,12 @@
+//Những điều cần lưu ý khi so sánh trong typescript
+
+// console.log(2 == '2');
+
+const obj = {
+  id: 1,
+  name: 'chris',
+};
+
+const cloneObj = obj;
+
+console.log(obj === cloneObj); //return true
